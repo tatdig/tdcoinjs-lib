936c2269f8a3eb891296ac3af220e8cc1a54e1a1
TATDig <32492514+tatdig@users.noreply.github.com>
2022-04-28 22:02:02 -0400

    Update networks.js

--- src/networks.js
+++ src/networks.js
@@ -2,34 +2,34 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.tdcoin = {
   messagePrefix: '\x18Tdcoin Signed Message:\n',
-  bech32: 'bc',
+  bech32: 'tc',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4,
   },
-  pubKeyHash: 0x00,
-  scriptHash: 0x05,
-  wif: 0x80,
+  pubKeyHash: 0x41,
+  scriptHash: 0x52,
+  wif: 0x6B,
 };
 exports.regtest = {
   messagePrefix: '\x18Tdcoin Signed Message:\n',
-  bech32: 'bcrt',
+  bech32: 'tdrt',
   bip32: {
     public: 0x043587cf,
     private: 0x04358394,
   },
   pubKeyHash: 0x6f,
-  scriptHash: 0xc4,
+  scriptHash: 0x5a,
   wif: 0xef,
 };
 exports.testnet = {
   messagePrefix: '\x18Tdcoin Signed Message:\n',
-  bech32: 'tb',
+  bech32: 'tt',
   bip32: {
     public: 0x043587cf,
     private: 0x04358394,
   },
   pubKeyHash: 0x6f,
-  scriptHash: 0xc4,
+  scriptHash: 0x44,
   wif: 0xef,
 };

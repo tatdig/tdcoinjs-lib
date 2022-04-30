3c88cb992237f6caabbe61a53b0f1690b4f8c9f3
TATDig <32492514+tatdig@users.noreply.github.com>
2022-04-29 21:15:07 -0400

    Update ecpair.js

--- src/ecpair.js
+++ src/ecpair.js
@@ -5,7 +5,7 @@ const types = require('./types');
 const ecc = require('tiny-secp256k1');
 const randomBytes = require('randombytes');
 const typeforce = require('typeforce');
-const wif = require('wif');
+const wif = require('wif-tdcoin');
 const isOptions = typeforce.maybe(
   typeforce.compile({
     compressed: types.maybe(types.Boolean),
